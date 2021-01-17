26e6856e62448e11ebd5846fd2cfd1cc97074f19
Gabriel Amaral <amaralgabriel123@gmail.com>
2021-01-16 21:04:30 -0300

    WebApplication Test

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Usuario usuario = new Usuario();
+            usuario.Name = "Gabriel";
+
+            return View(usuario);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
